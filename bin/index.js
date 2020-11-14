21820af289b1a9167b420156dcd51958d3c80f1d
AKP-13 <alexpeirson@hotmail.com>
2020-11-14 11:03:50 +0000

    Splice agruments, check for valid format, handle error of not including '--length'

--- bin/index.js
+++ bin/index.js
@@ -3,11 +3,29 @@
 const colors = require("colors");
 const convert = require("../lib/convert");
 
-// print random greeting
-console.log(
-    // wraps text with rainbow color formatting
-    colors.brightBlue(
-        // returns the random greeting text
-        convert.randomUnit()
-    )
-);
+// get arguments after first two elements in process.argv
+var arguments = process.argv.splice(2);
+
+// default values are null
+let fromValue = null;
+let fromUnit = null;
+let toUnit = null;
+
+// check if user wants length
+if (arguments[0] == "--length") {
+    // assign respective arguments
+    fromValue = arguments[1];
+    fromUnit = arguments[2];
+    toUnit = arguments[4];
+}
+
+// if valid input format, convert
+if (fromValue && fromUnit && toUnit) {
+    console.log(
+        colors.brightCyan(convert.convert(fromValue, fromUnit, toUnit))
+    );
+} else {
+    console.log(colors.brightRed(convert.randomUnit()));
+}
+
+// console.log(process.argv);
